e711bd38527b3258ec576d13b6e6aa8224036551
rmitache <rmitache@student.42heilbronn.de>
2024-04-19 10:48:53 +0200

    Added the first exercise of the course and made some notes about compiling. I already know this stuff but i gotta do what i gotta do

---
+++ Getting Started/Writing our first program/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+  int favorite_number;
+
+  std::cout << "Enter your favorite number between 0 and 100" << std::endl;
+  std::cin >> favorite_number;
+  std::cout << "Your favorite number is " << favorite_number << std::endl;
+}
